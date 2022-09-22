b272ef0096e4fa9546e466929dfbbef707999380
Ayman Eldawy <aymaneldawy@yahoo.com>
2022-09-22 19:10:11 +0200

    update home page file js

--- js/home-page.js
+++ js/home-page.js
@@ -21,10 +21,6 @@ const PrayerTimeDefault = {
 };
 let LOCATION = storageLocation.fetchLocation() || PrayerTimeDefault;
 
-let yearCalc = parseInt(
-  a2e(new Date().toLocaleDateString("ar-SA", { year: "numeric" }))
-);
-
 function resetDate(theNewDate) {
   let sunCalc = SunCalc.getTimes(
     new Date(),
@@ -53,6 +49,9 @@ function resetDate(theNewDate) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  let yearCalc = parseInt(
+    a2e(new Date().toLocaleDateString("ar-SA", { year: "numeric" }))
+  );
   let theHours = new Date().getHours();
   document.getElementById("date").textContent = `${resetDate(
     new Date()
@@ -81,6 +80,128 @@ window.addEventListener("DOMContentLoaded", () => {
   );
   document.getElementById("bigCentury").innerHTML = parseInt(yearCalc / 210);
   prayerTimingDay();
+
+  // Display first day in every month
+  for (let i = 1; i <= 12; i++) {
+    let _hijri = new HijriDate(yearCalc, i, 1);
+    let gregorian = _hijri.toGregorian();
+    let GregorianDateIncrement = new Date(gregorian);
+    document.querySelector(".row-first-days").innerHTML += `
+      <div>
+        <p>الاول من ${months[_hijri._month]}</p>
+        <p>${GregorianDateIncrement.toLocaleDateString("ar-EG", {
+          weekday: "long",
+        })}</p>
+        <p>${GregorianDateIncrement.toLocaleDateString("ar-EG", {
+          day: "numeric",
+          month: "long",
+        })}</p>
+      </div>
+    `;
+  }
+  // display the closest events
+  let eventsContainer = document.querySelector(".table-grid");
+  let MAX_STEPS = 0;
+  for (let index = 0; index < globalEvents.length; index++) {
+    if (
+      !(Date.parse(new Date()) > Date.parse(globalEvents[index].date)) &&
+      MAX_STEPS < 3
+    ) {
+      MAX_STEPS++;
+      eventsContainer.innerHTML += `
+      <div class="table-style-grid">
+        <h4>${globalEvents[index].title}</h4>
+        <span class="timer-style timer">${calculateDate(
+          globalEvents[index].date
+        )}</span>
+      </div>`;
+    }
+  }
+  // Fasting
+  let theYear = theCurrentDate.getCurrentYearHijri();
+  const yearNumber = theYear % 210;
+  // Loop of month days
+  function getDaysMonth(month) {
+    let FastingDays = [];
+    let monthCount = countDayOfMonth(month, yearNumber);
+
+    for (let i = 1; i <= monthCount; i++) {
+      let startLastThr = monthCount > 29 ? 24 : 23;
+      let _hijri = new HijriDate(theYear, month, i);
+      let gregorian = _hijri.toGregorian();
+      let GregorianDateIncrement = new Date(gregorian);
+      let weekDay = GregorianDateIncrement.toLocaleDateString("ar-SA", {
+        weekday: "long",
+      });
+      if (weekDay === "الخميس" && i <= 7) {
+        FastingDays.push({
+          title: "اول خميس",
+          date: GregorianDateIncrement,
+          month: months[month],
+        });
+      }
+      if (weekDay === "الأربعاء" && i >= 12 && i <= 18) {
+        FastingDays.push({
+          title: "اوسط أربعاء",
+          date: GregorianDateIncrement,
+          month: months[month],
+        });
+      }
+      if (weekDay === "الخميس" && i >= startLastThr && i <= monthCount) {
+        FastingDays.push({
+          title: "أخر خميس",
+          date: GregorianDateIncrement,
+          month: months[month],
+        });
+      }
+    }
+    return FastingDays;
+  }
+  let fastingListYear = [];
+  for (let i = 1; i <= 12; i++) {
+    if (i === 9) continue;
+    fastingListYear.push(...getDaysMonth(i));
+  }
+
+  let stepOfCondition = 0;
+  let fastGrid = document.querySelector(".fasting-grid");
+  let fastingContainer = document.querySelector("#closestFasting .table-grid");
+  for (let index = 0; index < fastingListYear.length; index++) {
+    // three closest fasting days
+    if (
+      Date.parse(fastingListYear[index].date) > Date.parse(new Date()) &&
+      stepOfCondition < 3
+    ) {
+      stepOfCondition++;
+      fastingContainer.innerHTML += `<div class= "table-style-grid">
+          <h4>${fastingListYear[index].title} <small>${
+        fastingListYear[index].month
+      }</small> </h4>
+          <span class="timer-style timer ${
+            Date.parse(fastingListYear[index].date) > Date.parse(new Date())
+              ? ""
+              : "completed"
+          } ">${calculateDate(fastingListYear[index].date)}</span>
+        </div>`;
+    }
+    // All fasting days in year
+    let date = new Date(fastingListYear[index].date).toLocaleDateString(
+      "ar-SA",
+      {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }
+    );
+    fastGrid.innerHTML += `
+      <div class="fasting-item">
+        <span>${calculateDate(fastingListYear[index].date)}</span>
+        <p>${fastingListYear[index].title}</p>
+        <span>${date}</span>
+      </div>
+      
+    `;
+  }
 });
 
 window.addEventListener("click", (e) => {
